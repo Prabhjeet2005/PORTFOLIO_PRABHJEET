b0ab6b1b5dfc20c07407aaf55731867c7fd8bf07
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-07-22 16:16:03 +0530

    Initial Commit

---
+++ src/Components/Navbar/NavbarComponent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NavbarComponent = () => {
+	return (
+    <>
+    <section>
+      <section>Home</section>
+      <section>About Me</section>
+      <section>Education</section>
+      <section>Skills</section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </section>
+    </>
+  )
+};
+
+export default NavbarComponent;
