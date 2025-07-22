ce1d1e0fdf40d9f2c1f5a453e2657eca5cc041d7
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-07-22 17:58:29 +0530

    Responsive Navbar Completed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
+
 import App from "./App";
 
 import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
+import "./index.css"
 
 const router = createBrowserRouter([
 	{
